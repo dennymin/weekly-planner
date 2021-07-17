44608a9d760b58d125a4c9dd3f3f109973696c2b
Denny Min <denden223@gmail.com>
2021-07-16 17:31:50 -0700

    Hi

--- main.js
+++ main.js
@@ -55,3 +55,13 @@ function daySelect(event) {
     }
   }
 }
+
+function organizeEntriesOldestToYoungest(date) {
+  var organizedData = [];
+  var oldestEntry = 0;
+  for (var dayEntryIndex = 0; dayEntryIndex < date.length; dayEntryIndex++) {
+    if (data.day[date][dayEntryIndex] > oldestEntry) {
+
+    }
+  }
+}
